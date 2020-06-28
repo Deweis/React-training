8ef6ba52275ddf0c073f953f17c7aa9ea844b302
Edgar Balsis <edgarbalsis1994@gmail.com>
2020-06-28 15:38:11 +0300

    2nd version

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import Header from './components/Header/Header.jsx';
 import Sidebar from './components/Sidebar/Sidebar.jsx';
 import Profile from './components/Profile/Profile.jsx';
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter,Route} from "react-router-dom";
 
 
 const App = () => {
-  return (
-    <div className="wrapper">
-        <Header />
-        <Sidebar />
-        <div className="content-wrapper">
-            <Profile />
-        </div>
-        <Dialogs />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="wrapper">
+                <Header/>
+                <Sidebar/>
+                <div className="content-wrapper">
+                    <Route path="/dialogs" component={Dialogs}/>
+                    <Route path="/profile" component={Profile}/>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
+        ;
 }
 
 export default App;
